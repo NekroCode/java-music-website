93da9b23a53cd631f3296a72bce935a6fd10af52
NekroCode <nekrojunk@hotmail.com>
2020-01-17 19:19:30 +0100

    Laatste wijziging stond nog niet online

--- src/users/LoginController.java
+++ src/users/LoginController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.mindrot.jbcrypt.BCrypt;
 
-@WebServlet("/login")
+@WebServlet("/Login")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
